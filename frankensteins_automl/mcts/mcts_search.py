f46ef48e0e5e6134109d70a923eb852c56f754f4
Berberer <lukas.brandt@veds.de>
2019-10-24 17:34:06 +0200

    finished first mcts prototype

--- frankensteins_automl/mcts/mcts_search.py
+++ frankensteins_automl/mcts/mcts_search.py
@@ -14,7 +14,7 @@ class RandomSearchSimulation:
         self.start_node = start_node
         self.graph_generator = graph_generator
 
-    def perform_simulation(self, optimization_time_budget):
+    async def perform_simulation(self, optimization_time_budget):
         current_node = self.start_node
         optimization_leaf = None
         while optimization_leaf is None:
@@ -24,10 +24,10 @@ class RandomSearchSimulation:
                 if current_node.get_successors() is None:
                     self.graph_generator.generate_successors()
                 current_node = random.choice(current_node.get_successors())
-        score = optimization_leaf.perform_optimization_simulation(
+        result, score = await optimization_leaf.perform_optimization(
             optimization_time_budget
         )
-        return optimization_leaf, score
+        return optimization_leaf, result, score
 
 
 class MctsSearchConfig:
@@ -54,7 +54,6 @@ class MctsSearch:
             self.search_space, self.config.start_component_name, optimizers
         )
         self.root_node = self.graph_generator.get_root_node()
-        self.search_front = [self.root_node]
         self.best_candidate = None
         self.best_score = 0.0
 
@@ -72,19 +71,59 @@ class MctsSearch:
         return self.best_candidate, self.best_score
 
     async def _search_awaitable(self):
-        candidate_node = self._select_candidate_node()
-        expanded_nodes = self._candidate_node_expansion(candidate_node)
-        leaf_nodes = self._simulation_of_expanded_nodes(expanded_nodes)
-        self._back_propagation(leaf_nodes)
+        while True:
+            candidate_node = self._select_candidate_node()
+            expanded_nodes = self._candidate_node_expansion(candidate_node)
+            leaf_nodes = await self._simulation_of_expanded_nodes(
+                expanded_nodes
+            )
+            self._back_propagation(leaf_nodes)
 
     def _select_candidate_node(self):
-        pass
+        current_node = self.root_node
+        while (
+            current_node.get_successors() is not None
+            and len(current_node.get_successors()) > 0
+        ):
+            successors = current_node.get_successors()
+            current_node = successors[0]
+            best_score = current_node.get_node_value()
+            for successor in successors:
+                if successor.get_node_value() > best_score:
+                    current_node = successor
+                    best_score = successor.get_node_value()
+        logger.info(f"Nex candidate for expansion is {current_node}")
+        return current_node
 
     def _candidate_node_expansion(self, candidate_node):
-        pass
+        return self.graph_generator.generate_successors(candidate_node)
 
-    def _simulation_of_expanded_nodes(self, expanded_nodes):
-        pass
+    async def _simulation_of_expanded_nodes(self, expanded_nodes):
+        optimized_leafs = []
+        random_searches = []
+        for node in expanded_nodes:
+            for i in range(self.config.simulation_runs_amount):
+                random_searches.append(
+                    RandomSearchSimulation(node, self.graph_generator)
+                )
+        logger.info(f"Start random search simulations")
+        for simulation_result in asyncio.as_completed(
+            [rs.perform_simulation() for rs in random_searches]
+        ):
+            leaf, result, score = await simulation_result
+            leaf.recalculate_node_value(score)
+            optimized_leafs.append(leaf)
+            if score > self.best_score:
+                logger.info(f"New best candidate is {result} with {score}")
+                self.best_candidate = result
+                self.best_score = score
+        return optimized_leafs
 
     def _back_propagation(self, leaf_nodes):
-        pass
+        nodes = [(node, node.get_score_avg()) for node in leaf_nodes]
+        while len(leaf_nodes) > 0:
+            node, score = nodes.pop(0)
+            node.recalculate_node_value(score)
+            predecessor = node.get_predecessor()
+            if predecessor is not None:
+                node.append((predecessor, node.get_score_avg()))
--- frankensteins_automl/mcts/mcts_search_graph.py
+++ frankensteins_automl/mcts/mcts_search_graph.py
@@ -1,4 +1,5 @@
 import logging
+import math
 from frankensteins_automl.search_space.search_space_graph import (
     SearchSpaceGraphNode,
     SearchSpaceGraphGenerator,
@@ -13,6 +14,7 @@ class MctsGraphNode(SearchSpaceGraphNode):
         self.optimizer = optimizer
         self.node_value = 0
         self.simulation_visits = 0
+        self.score_avg = 0
 
     def is_search_space_leaf_node(self):
         return super.is_leaf_node()
@@ -26,16 +28,25 @@ class MctsGraphNode(SearchSpaceGraphNode):
     def get_node_value(self):
         return self.node_value
 
-    def set_node_value(self, node_value):
-        self.node_value = node_value
+    def get_score_avg(self):
+        return self.score_avg
 
     def get_simulation_visits(self):
         return self.simulation_visits
 
-    def increase_simulation_visits(self):
+    def recalculate_node_value(self, new_result):
         self.simulation_visits = self.simulation_visits + 1
+        self.score_avg = self.score_avg + (
+            (new_result - self.score_avg) / self.simulation_visits
+        )
+        exploration_factor = 1.41421 * math.sqrt(
+            math.log(float(self.predecessor.get_simulation_visits()))
+            / float(self.simulation_visits)
+        )
+        self.node_value = self.score_avg + exploration_factor
+        pass
 
-    def perform_optimization_simulation(self, time_budget):
+    async def perform_optimization(self, time_budget):
         pass
 
 
