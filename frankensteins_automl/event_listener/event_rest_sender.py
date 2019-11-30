dff6975db6d5daabc544bf30f41d7faefeaa943c
Berberer <lukas.brandt@veds.de>
2019-11-30 01:55:49 +0100

    will stop sending events in case of error

--- frankensteins_automl/event_listener/event_rest_sender.py
+++ frankensteins_automl/event_listener/event_rest_sender.py
@@ -5,15 +5,23 @@ from frankensteins_automl.event_listener import event_topics
 
 relevant_topics = [event_topics.MCTS_TOPIC, event_topics.SEARCH_GRAPH_TOPIC]
 url = None
+is_sending = False
 
 
 def activate(rest_url):
     global url
+    global is_sending
     url = rest_url
+    is_sending = True
     for topic in relevant_topics:
         pub.subscribe(_listener, topic)
 
 
 def _listener(payload, topic=pub.AUTO_TOPIC):
+    global is_sending
     payload["message_id"] = str(uuid.uuid1())
-    requests.post(url, json=payload)
+    if is_sending:
+        try:
+            requests.post(url, json=payload)
+        except Exception:
+            is_sending = False
